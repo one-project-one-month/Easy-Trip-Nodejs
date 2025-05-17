11ed700571a3cfd2fd4e0a848e70043e9da61602
Ast_Zinmoag <111127377+Zinmoeag@users.noreply.github.com>
2025-05-17 11:09:09 +0630

    chore: install axios

---
+++ src/routes/planner.route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { StatusCode } from "../utils/Status";
+import validationMiddleware from "../middleware/validation.middleware";
+import { RegisterCredentialSchema, loginCredentialSchema } from "../feature/auth/api/body";
+
+const router = Router();
+
+router.get("/thring-you-should-know", (req, res) => { res.sendStatus(StatusCode.OK) })
+    // .post('/login', validationMiddleware.validateRequestBody(loginCredentialSchema), (req, res) => { })
+    // .post('/register', validationMiddleware.validateRequestQuery(RegisterCredentialSchema), (req, res) => { })
+    // .post('/logout', (req, res) => { })
+    // .post('/refresh', (req, res) => { })
+
+export default router;
